88b27eb5a0fa1940125c196685c434fafaa8fbd8
StefanoMT20 <pieromezatejada14@hotmail.com>
2026-01-19 18:07:34 -0500

    changes

--- apps/finances/admin.py
+++ apps/finances/admin.py
@@ -13,17 +13,17 @@ class BankAccountAdmin(admin.ModelAdmin):
 
 @admin.register(CreditCard)
 class CreditCardAdmin(admin.ModelAdmin):
-    list_display = ['name', 'user', 'last_four_digits', 'limit', 'used', 'created_at']
-    list_filter = ['color', 'created_at', 'user']
+    list_display = ['name', 'user', 'last_four_digits', 'limit', 'currency', 'used_pen', 'used_usd', 'created_at']
+    list_filter = ['currency', 'color', 'created_at', 'user']
     search_fields = ['name', 'last_four_digits']
-    readonly_fields = ['id', 'used', 'created_at', 'updated_at']
+    readonly_fields = ['id', 'used_pen', 'used_usd', 'created_at', 'updated_at']
     ordering = ['-created_at']
 
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ['description', 'user', 'amount', 'category', 'credit_card', 'date', 'created_at']
-    list_filter = ['category', 'credit_card', 'date', 'created_at', 'user']
+    list_display = ['description', 'user', 'amount', 'currency', 'category', 'credit_card', 'bank_account', 'date', 'created_at']
+    list_filter = ['currency', 'category', 'credit_card', 'bank_account', 'date', 'created_at', 'user']
     search_fields = ['description']
     readonly_fields = ['id', 'created_at']
     ordering = ['-date', '-created_at']
