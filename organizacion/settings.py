14275449ea206ade714f419e9f5687d8271230b2
StefanoMT20 <pieromezatejada14@hotmail.com>
2026-01-26 19:52:20 -0500

    changes

--- organizacion/settings.py
+++ organizacion/settings.py
@@ -129,6 +129,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 500,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
